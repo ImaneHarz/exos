a134905ff3ec709643b2d04c10668c56222d5b5a
ImaneHarz <65900723+ImaneHarz@users.noreply.github.com>
2020-09-28 16:28:40 +0200

    Create syncCopy.js

---
+++ syncCopy.js
@@ -0,0 +1,10 @@
+/* Ecrivez un programme qui copie le fichier
+ file1.txt vers le fichier file2.txt 
+ en utilisant les api synchrones du module
+  fs de node.js */
+
+import fs from 'fs'
+
+let content = fs.copyFileSync('file1.txt', 'file2.txt')
+console.log('file1.txt was copied to file2.txt')
+git
